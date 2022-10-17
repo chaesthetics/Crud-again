d57632602ac8c6549170891ba155d9f473c86e76
Auriel James Fernandez <61530799+chaesthetics@users.noreply.github.com>
2022-10-17 15:29:12 +0800

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,45 +1,54 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors')
-const { default: mongoose } = require('mongoose')
 
 var app = express('')
 app.use(cors())
 app.use(express.json())
-
 const PORT = 3001
 
-const UserModel = require('./models/Student')
-
-mongoose.connect("mongodb+srv://aurieljames11:admin123@cluster0.gtws5hg.mongodb.net/test",{
-    useNewURLParser: true,
+const studentModel = require('./models/Student')
+const student = require('./models/Student')
+mongoose.connect('mongodb+srv://aurieljames11:admin123@cluster0.gtws5hg.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
 })
 
-app.post('/insert', async (req, res)=>{
 
-    const studId = req.body.studentId
+
+app.post('/insert', async (req, res)=>{
+    const studentId = req.body.studentId
     const fname = req.body.fname
     const course = req.body.course
     const section = req.body.section
-    const user = new UserModel({studentId: studId,fullName: fname, course: course, section: section})
 
-    try{
-        await user.save()
-        res.send("inserted")
-    }
-    catch(err){
-        console.log(err)
-    }
+
+   const user = new studentModel({studentId: studentId, fullName: fname, course: course, section: section})
+
+   try{
+    await
+    user.save()
+    res.send('inserted')
+   }
+   catch(err){
+    console.log(err)
+   }
 })
 
 app.get('/read', async (req, res)=>{
-    UserModel.find({}, (err, result)=>{
+    studentModel.find({}, (err, result)=>{
         if(err){
             console.log(err)
         }
-        res.send(result)
+    res.send(result)
     })
 })
 
+app.delete('/delete/:id', async (req, res)=>{
+    const id = req.params.id;
+
+    await studentModel.findByIdAndRemove(id).exec();
+    res.send('deleted');
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running in port ${PORT}`)
